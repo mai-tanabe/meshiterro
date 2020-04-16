e020e9e45036e20efb478d7037f7d1219c7e2cca
mai-tanabe <tick.tack.way@gmail.com>
2020-04-16 03:36:40 +0000

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f8cab13d828d482c05b9f180b39421eb40bd19c9300d3c30fd91a3e71ee0933f9835ba778720d5859341ed61ae95d72fc0c42531c3d65457e7ce06965745ae3c'
